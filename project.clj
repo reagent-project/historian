e8a15fe77f863b2f7879af84b793b43a7329ee48
Frozenlock <frozenlock@gmail.com>
2014-04-12 15:38:21 -0400

    project.clj info updated

--- project.clj
+++ project.clj
@@ -1,6 +1,13 @@
 (defproject historian "1.0.1-SNAPSHOT"
   :description "Automatically save atoms and restore their previous states if needed."
-  ;:url "http://example.com/FIXME"
+  ;:url "https://github.com/Frozenlock/historian"
+  :scm {:name "git"
+         :url "https://github.com/Frozenlock/historian"})
+
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
 
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2138"]]
