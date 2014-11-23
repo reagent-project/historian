f2f002ec0e73965d1445a0a1f627dff1eccf5546
Frozenlock <frozenlock@gmail.com>
2014-11-23 12:33:42 -0500

    cljx moved back outside of :dev, otherwise it doesn't generate the files when deploying

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject historian "1.0.6"
+(defproject historian "1.0.7"
   :description "Automatically save atoms and restore their previous states if needed."
   :url "https://github.com/Frozenlock/historian"
   :scm {:name "git"
@@ -12,25 +12,22 @@
   :dependencies [[org.clojure/clojurescript "0.0-2371" :scope "provided"]]
 
 
-  :profiles {:dev {:plugins [[org.clojars.frozenlock/cljx "0.4.6"]]
-                   :hooks [cljx.hooks]
-                   :cljx {:builds [{:source-paths ["src/cljx"]
-                                    :output-path "target/classes"
-                                    :rules :clj}
-                                   
-                                   {:source-paths ["src/cljx"]
-                                    :output-path "target/classes"
-                                    :rules :cljs}
-                                   
-                                   {:source-paths ["test/cljx"]
-                                    :output-path "target/test"
-                                    :rules :clj}
-                                   
-                                   ;; {:source-paths ["test/cljx"]
-                                   ;;  :output-path "target/test"
-                                   ;;  :rules :cljs}
-                                   ]}
-                   }}
+  :profiles {:dev {:plugins [[org.clojars.frozenlock/cljx "0.4.6"]]}}
+
+  :hooks [cljx.hooks]
+  :cljx {:builds [{:source-paths ["src/cljx"]
+                   :output-path "target/classes"
+                   :rules :clj}
+                  
+                  {:source-paths ["src/cljx"]
+                   :output-path "target/classes"
+                   :rules :cljs}
+                  
+                  {:source-paths ["test/cljx"]
+                   :output-path "target/test"
+                   :rules :clj}]}
+                
+
 
   :plugins [[lein-cljsbuild "1.0.3"]]
 
