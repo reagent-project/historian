3fb87210fec4fe80a9ddaed10334d9f2b3463d65
Frozenlock <frozenlock@gmail.com>
2019-05-01 11:35:47 -0400

    Bump version to 1.2.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject historian "1.2.1"
+(defproject historian "1.2.2"
   :description "Automatically save atoms and restore their previous states if needed."
   :url "https://github.com/Frozenlock/historian"
   :scm {:name "git"
